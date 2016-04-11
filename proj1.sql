59d490eebedb6700a302d3c57a054a6a67e6a5a1
Yu <97508691@qq.com>
2016-04-11 17:13:44 +1000

    contain the first and second Question

---
+++ proj1.sql
@@ -0,0 +1,24 @@
+ create or replace view Q1(unswid,name)
+ AS
+ SELECT DISTINCT people.unswid,
+  	         people.name
+   FROM people,
+    course_enrolments
+  WHERE course_enrolments.course > 55 AND course_enrolments.student = people.unswid;
+
+create or replace view Q2_1
+as
+select *
+FROM  affiliations
+WHERE affiliations.ending IS NULL 
+AND affiliations.role = (SELECT staff_roles.id
+									FROM staff_roles 
+									WHERE staff_roles.name = 'Head of School');
+
+create or replace view Q2(name,school,starting)
+as
+select people.name,orgunits.longname,Q2_1.starting
+from Q2_1,people,orgunits
+where people.id=Q2_1.staff
+and	  Q2_1.orgunit=orgunits.id
+and   Q2_1.isprimary='t'
