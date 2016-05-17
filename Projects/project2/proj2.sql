8f747c4e142e7e7cca0fc87ea873d7f8569d4b35
Yu <97508691@qq.com>
2016-05-17 23:08:30 +1000

    checked

--- Projects/project2/proj2.sql
+++ Projects/project2/proj2.sql
@@ -16,7 +16,7 @@ select mark,grade into fenshu,chenji
 from course_enrolments
 where student = $1 
 and course = $2;
-if fenshu is not null and (chenji = 'SY' or chenji = 'PC' or chenji = 'PS'
+if fenshu is  null or (chenji = 'SY' or chenji = 'PC' or chenji = 'PS'
 or chenji = 'CR' or chenji = 'DN' or chenji = 'HD' or chenji ='PT' or chenji = 'A' or chenji = 'B') 
 then 
  select uoc into r
@@ -29,7 +29,7 @@ then
  end if;
  end;
 $$
-language plpgsql
+language plpgsql;
 ----------------------------
 
 ------------------------
@@ -59,16 +59,15 @@ $$ language sql;
 ----------------------
 
 --------------------
-create or replace view Q1_all
-as
-select course_enrolments.student,
-course_enrolments.course,
-course_enrolments.mark,
-course_enrolments.grade,
-name
-subject,
-semester 
-from (courses left join course_enrolments on course = courses.id)
+CREATE OR REPLACE VIEW q1_all AS 
+ SELECT course_enrolments.student,
+    course_enrolments.course,
+    course_enrolments.mark,
+    course_enrolments.grade,
+    courses.subject,
+    courses.semester
+   FROM courses
+     LEFT JOIN course_enrolments ON course_enrolments.course = courses.id;
 -------------
 create or replace function Q1_name(int)
 returns text
@@ -82,22 +81,23 @@ create or replace function Q1_rank(ke integer)
 returns table(student integer,rank bigint)
 as $$
 select student,rank() over (order by mark DESC)from course_enrolments
-where course = ke; 
-$$ language sql
+where course = ke
+and mark is not null; 
+$$ language sql;
 ----------------
 create or replace function Q1_program_code(int)
 returns char(4)
 as $$
 select code from programs
 where id = $1;
-$$ language sql
+$$ language sql;
 ----------------
 create or replace function Q1_student_rank(stu integer, ke integer)
 returns bigint
 as $$
 select rank from Q1_rank(ke)
 where student = stu; 
-$$ language sql
+$$ language sql;
 -----------------------------
 create or replace function Q1_total(integer)
 returns bigint
@@ -106,9 +106,8 @@ select count(*) from course_enrolments
 where mark is not null 
 and course = $1
 $$
-language sql
+language sql;
 ---------------------
-
 ---------------------------------
 create type TranscriptRecord as (code char(8), term char(4), course integer, prog char(4), name text, mark integer, grade char(2), uoc integer, rank integer, totalEnrols integer);
 
@@ -137,7 +136,7 @@ and Q1_all.student = program_enrolments.student loop
 return next r;
 end loop;
 return;
-end
+end;
 $$
 language plpgsql;
 
@@ -179,7 +178,7 @@ begin
 			end loop;
 			return;
 end;
-$$ language plpgsql
+$$ language plpgsql;
  
 -- Q3: ...
 -------------------
@@ -187,55 +186,16 @@ create or replace function Q3_staff_role(int)
 returns text
 as $$
 	select name from staff_roles where id = $1
-     $$ language sql
+     $$ language sql;
 ---------------------
 create or replace function Q3_org_name(int)
 returns char(64)
 as $$
 	select name from orgunits where id = $1
-     $$ language sql
+     $$ language sql;
 -----------------------
-create or replace function Q3_person(integer) 
-	returns setof EmploymentRecord 
-as $$
- declare person int;
-	       n int;
-	       emp record;
-	       emp2 record;
-	       t int :=0;
-	       result text := '';
-	       cur_end text := ' ';
-	     
 
- begin 
-	for person in select staff from affiliations where orgunits = $1 group by (staff)
-	loop 
-	        execute 'select count(*) from affiliations '||' where staff = '||person into n ; 	     
-	        if n > 1 then		
-			for emp in select * from affiliations where staff = person order by starting 
-				loop
-				if (cur_end != ' ') then
-					if cur_end > emp.end then
-						
-				
-				
-				
-				end loop;
-			if t > 1 then
-				for a in select * from affiliations where staff = person order by starting
-				loop
-					if a.starting is null then
-						result = result ||Q3_staff_role(a.staff)||', '||Q3_org_name(a.orgunit)||'('||a.starting||'..)'||E'\n'
-					else
-						result = result ||Q3_staff_role(a.staff)||', '||Q3_org_name(a.orgunit)||'('||a.starting||'..'||a.ending||')'||E'\n'
-				end loop;
-			end if;
-			select Q1_id(staff), staff, result into EmploymentRecord
-			return next EmploymentRecord;
-		end if;
-	end loop;
-end					
-$$ language plpgsql;
+
 --------------test--------------------------
 create or replace function Q3_show(int)
 returns setof EmploymentRecord
@@ -248,44 +208,18 @@ begin
 	for a in select * from affiliations where  staff = $1  order by starting
 	loop
 		if a.ending is null then
-			result := '1'||result||'Q3_staff_role('||a.staff||')'||a.starting||E'\n';
-			
+			result := result||Q3_staff_role(a.role)||', '||Q3_org_name(a.orgunit)||' ('||a.starting||'..)'||E'\n';	
 		else
-			result := '2'||result||a.starting||E'\n';
-		end if;
-		raise notice 'Value: %', result;
-	end loop;
-	result := result||'333';
-	select Q3_unswid($1), Q3_name($1), result into employ;
-	return next employ;
-end
-$$ language plpgsql
-----------------------------------
-create or replace function Q3_show(int)
-returns setof EmploymentRecord
-as $$
-declare 
-a record;
-result text := '';
-employ EmploymentRecord;
-begin
-	for a in select * from affiliations where  staff = $1  order by starting
-	loop
-		if a.ending is null then
-			result := result||Q3_staff_role(a.role)||', '||Q3_org_name(a.orgunit)||'('||a.starting||'..)'||E'\n';	
-		else
-			result := result||Q3_staff_role(a.role)||', '||Q3_org_name(a.orgunit)||'('||a.starting||'..'||a.ending||')'||E'\n';
+			result := result||Q3_staff_role(a.role)||', '||Q3_org_name(a.orgunit)||' ('||a.starting||'..'||a.ending||')'||E'\n';
 		end if;
 	end loop;
 	
 	select Q3_unswid($1), Q3_name($1), result into employ;
 	return next employ;
 end
-$$ language plpgsql
+$$ language plpgsql;
       
-	
-
-select * from q3_show(50409255)      
+	  
 ----------------------------------
 create or replace function Q3_name(int)
 returns char(128)
@@ -310,38 +244,7 @@ as $$
 		select p.member from included_org pr, orgunit_groups p where p.owner = pr.member)
 	select member from included_org union select $1
 $$ language sql;
------------------------------
-create or replace function Q3(integer) 
-	returns setof EmploymentRecord 
-as $$
- declare person int;
-	       n int;
-	       emp record ;
-	       next_date date := '1000-01-01';
-	       t int := 0;
-	       
 
- begin 
-	for person in select staff from affiliations where orgunit in (select * from Q3_org($1)) group by (staff)
-	loop 
-		t:=0;
-		for emp in select * from affiliations where staff = person order by starting
-		loop
-			if next_date != '1000-01-01' then
-				if next_date <= emp.starting then
-					t := t+1;
-				end if;
-			end if;
-			if next_date < emp.ending then
-				next_date := emp.ending;
-			end if;
-		end loop;
-		if t >1 then
-		return next Q3_show(person);
-		end if;
-	end loop;
-end;
-$$ language plpgsql;
 -----------------------------
 
 
@@ -351,27 +254,37 @@ create or replace function Q3(integer)
 as $$
  declare person int;
 	       n int;
-	       emp record;
-	       emp2 record;
-	       t int :=0;
+	       emp record ;
+	       next_date date ;
+	       t int := 0;
+	       
 
  begin 
-	for person in select staff from affiliations where orgunit in (select * from Q3_org(661)) group by (staff)
-	loop 
-	        execute 'select count(*) from affiliations '||' where staff = '||person into n ; 	     
-	        if n > 1 then
-			for emp in select * from affiliations where staff = person
-			loop
-				for emp2 in select * from affiliations where staff = person
-				loop
-					if (emp.starting, emp.ending) OVERLAPS (emp2.starting, empo2.ending) then
-						t := t+1;
-				end loop
-			end loop
-			if t > 1 then
-				return next 
+	for person in select staff from affiliations where orgunit in (select * from Q3_org($1)) group by (staff)
+	loop 		
+		next_date := null;
+		t := 0;
+		for emp in select * from affiliations where staff = person and orgunit in (select * from Q3_org($1)) order by starting 
+		loop
+			if next_date is not null then
+				if next_date <= emp.starting then
+					t := t+1;
+				end if;
+			end if;
+			if next_date is null then
+				next_date := emp.ending;
+			end if;		
+			if next_date < emp.ending then
+				next_date := emp.ending;
+			end if;
+		end loop;
+		if t > 0 then
+		return next Q3_show(person);
+		end if;
+	end loop;
+end;
+$$ language plpgsql;
 					
 				
-	
-$$ language plpgsql;
+
 
